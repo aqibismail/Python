a1269cc6b77b20771ec9640208328f03133a6f64
aqibismail <aqib@kikitproject.org>
2023-06-26 14:13:48 +0100

    Update tempconverter.py
    Using functions to improve reliability. Added multiple if elif statements for accuracy.

--- tempconverter.py
+++ tempconverter.py
@@ -1,20 +1,26 @@
 convert = input ("What would you like to convert? \n A. C-F \n B. F-C \n Select option A or B: ")
 
-if convert == 'a' or 'A':
+def celcius():
     celcius = input ("Temperature? (C): ")
     cel_to_float = float(celcius)
-
-    temp = ((cel_to_float * 9 / 5) + 32)
+    temp  = ((cel_to_float * 9 / 5) + 32)
     temp_to_str = str(temp)
-
     print(temp_to_str + "°F")
-elif convert == 'b' or 'B':
+
+def farenheit():
     farenheit = input ("Temperature? (F): ")
     far_to_float = float(farenheit)
-
     temp = ((far_to_float - 32) * 5 / 9)
     temp_to_str = str(temp)
-
     print(temp_to_str + "°C")
+
+if convert == 'a':
+    celcius()
+elif convert == 'A':
+    celcius()
+elif convert == 'b':
+    farenheit()
+elif convert == 'B':
+    farenheit()
 else:
-    print("Error 100: Please select option A or B")
+    print("Error, please select either option A or B")
