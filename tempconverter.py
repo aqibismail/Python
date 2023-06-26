45557808c7b7271f7b79d307ff51f07854648f55
aqibismail <aqib@kikitproject.org>
2023-06-26 21:17:05 +0100

    updates

--- tempconverter.py
+++ tempconverter.py
@@ -1,18 +1,25 @@
-convert = input ("What would you like to convert? \n A. C-F \n B. F-C \n Select option A or B: ")
+# Basic Temp Converter v.1.0.0
+
+def question():
+    global convert
+    convert = input ("What would you like to convert? \n A. Celcius-Farenheit \n B. Farenheit-Celcius \n Select option A or B: ")
+question()
+
 
 def celcius():
-    celcius = input ("Temperature? (C): ")
+    celcius = input ("Temperature? (°C): ")
     cel_to_float = float(celcius)
     temp  = ((cel_to_float * 9 / 5) + 32)
     temp_to_str = str(temp)
-    print(temp_to_str + "°F")
+    print(celcius + "°C is " + temp_to_str + "°F")
+
 
 def farenheit():
-    farenheit = input ("Temperature? (F): ")
+    farenheit = input ("Temperature? (°F): ")
     far_to_float = float(farenheit)
     temp = ((far_to_float - 32) * 5 / 9)
     temp_to_str = str(temp)
-    print(temp_to_str + "°C")
+    print(farenheit + "°F is " + temp_to_str + "°C")
 
 if convert == 'a':
     celcius()
@@ -24,3 +31,4 @@ elif convert == 'B':
     farenheit()
 else:
     print("Error, please select either option A or B")
+
